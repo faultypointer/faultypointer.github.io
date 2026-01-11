183ab96ed62cb8d1310e4c77744bd9d04cb61843
faultypointer <faultypointer@proton.me>
2026-01-11 19:49:56 +0545

    deploy to gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://faultypointer.github.io',
+    // no base as it is for faultypointer.github.io
+});
