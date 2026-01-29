4d227d45dae99dfe51eb81ac972778e4625171b4
faultypointer <faultypointer@proton.me>
2026-01-29 08:59:51 +0545

    sidenote and marginnote

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://faultypointer.github.io',
+    integrations: [mdx()],
     markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex],
