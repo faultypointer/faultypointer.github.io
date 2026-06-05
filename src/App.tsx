0a2ebf08091051a4ca26163ba6a15964318b8d4f
faultypointer <faultypointer@proton.me>
2026-06-05 15:46:50 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-full bg-slate-800">
+      <p className="text-white p-4">PortfoliOS loading...</p>
+    </div>
+  )
+}
+
+export default App;
