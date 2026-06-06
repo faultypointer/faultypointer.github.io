fa38d0469e22d02a4a9aee9a8ea831387888f519
faultypointer <faultypointer@proton.me>
2026-06-06 08:34:47 +0545

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolios/",
+  base: "/PortFoliOS/",
   build: {
     outDir: "docs",
   },
