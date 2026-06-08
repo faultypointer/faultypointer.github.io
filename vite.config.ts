b5c7e7602cb8c8ffbacc7eb2641db30caf3cb16d
faultypointer <faultypointer@proton.me>
2026-06-08 11:04:46 +0545

    remote base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/PortFoliOS/",
+  // base: "/PortFoliOS/",
   build: {
     outDir: "docs",
   },
